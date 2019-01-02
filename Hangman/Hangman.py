6a856d9cde2b2fd271a59c7904134c93bad735a7
bradleymailbox <bradleymailbox@gmail.com>
2019-01-02 14:31:42 +0200

    First bit of code

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -4,4 +4,32 @@
 
 import codemodule
 
-codemodule.printmsg("Welcome to Hangman")
+codemodule.printmsg(0)
+
+# initialize variables
+letter =""
+numguesses = 0
+
+# empty lists
+guessed_letters = []
+correct_letters = []
+wrong_letters = []
+
+while numguesses < 5:
+    # give the user the opportunity to guess
+    codemodule.printmsg(1)
+    letter= input()
+    # check if the user has guessed the currrent letter
+    if letter in guessed_letters:
+        codemodule.printmsg(2)
+    else:
+        guessed_letters.append(letter)
+        codemodule.printmsg(3)
+  
+    # if the letter entered is not in the list then increment the number of guesses
+
+    # add the letter to the list of wrong letters
+    
+    # else 
+    # build the list of correct guesses
+    # display the word with the letters guessed
--- Hangman/codemodule.py
+++ Hangman/codemodule.py
@@ -1,3 +1,16 @@
 # displays the string the function received
-def printmsg(s):
-    print s
+msgs = ['Welcome to Hangman',
+        'Please enter a letter you wish to guess:',
+        'You have already guessed that letter',
+        'Validating the letter entered',
+        'Number of guesses made:'
+    
+]
+
+# display a message from the msgs list
+def printmsg(m):
+    print msgs[m]
+
+# print the number of guesses the user has made
+def print_guesses(x):
+    print msgs[m]
