27a47e96f7f43b144d83e4f3045a4ca8591017d3
bradleymailbox <bradleymailbox@gmail.com>
2019-01-02 17:03:06 +0200

    Version 1

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -2,29 +2,37 @@
 # DATE: 2019
 # ABOUT: classic game of hangman
 
-import codemodule
+import codemodule as cm
 
-codemodule.printmsg(0)
+cm.printmsg(0)
 
 # initialize variables
 letter =""
 numguesses = 0
-
+totalwords = 0
+compword = ""
 # empty lists
 guessed_letters = []
-correct_letters = []
-wrong_letters = []
+words = []
+# get the words and count
+words = cm.getwords()
+totalwords = len(words)
+# get a random word from the data file
+compword = words[cm.getRandomValue(totalwords)]
 
+# loop through the number of times user can guess
 while numguesses < 5:
+    #display the current message
+    print cm.printWord(compword, guessed_letters)
     # give the user the opportunity to guess
-    codemodule.printmsg(1)
-    letter= input()
+    cm.printmsg(1)
+    letter= raw_input()
     # check if the user has guessed the currrent letter
     if letter in guessed_letters:
-        codemodule.printmsg(2)
+        cm.printmsg(2)
     else:
         guessed_letters.append(letter)
-        codemodule.printmsg(3)
+        cm.printmsg(3)
   
     # if the letter entered is not in the list then increment the number of guesses
 
--- Hangman/codemodule.py
+++ Hangman/codemodule.py
@@ -1,12 +1,13 @@
-# displays the string the function received
-msgs = ['Welcome to Hangman',
+import random
+# variables
+dict = "words.txt"
+msgs = ['Welcome to Hangman - Lets play a round',
         'Please enter a letter you wish to guess:',
         'You have already guessed that letter',
         'Validating the letter entered',
         'Number of guesses made:'
     
 ]
-
 # display a message from the msgs list
 def printmsg(m):
     print msgs[m]
@@ -14,3 +15,25 @@ def printmsg(m):
 # print the number of guesses the user has made
 def print_guesses(x):
     print msgs[m]
+
+# get the list of words
+def getwords():
+    words = []
+    with open(dict) as file:
+        for line in file: 
+            line = line.strip() #or some other preprocessing
+            words.append(line) 
+    return words
+
+# get a random value : maximum passed 
+def getRandomValue(max):
+    return random.randint(1,max)
+
+#print the word
+def printWord(compword, guessed_letters):
+    for g in compword:
+        if g not in guessed_letters:
+            compword = compword.replace(g, "#")        
+    return compword
+    
+    
